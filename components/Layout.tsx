74b5a91b761f9a5d99435e07767577030653732a
seokjoon.yun <seokjoon.yun@gmail.com>
2021-02-05 09:45:42 +0900

    Layout component화

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,41 +1,81 @@
-import { ReactNode } from 'react'
-import Link from 'next/link'
 import Head from 'next/head'
+import { Layout } from 'antd'
+import { useState, ReactElement, ReactNode } from 'react'
+import Menu from '@components/Menu'
+
+const { Sider, Header, Footer, Content } = Layout
+
 
 type Props = {
   children?: ReactNode
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+// const Layout = ({ children, title = 'This is the default title' }: Props) => (
+//   <div>
+//     <Head>
+//       <title>{title}</title>
+//       <meta charSet="utf-8" />
+//       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+//     </Head>
+//     <header>
+//       <nav>
+//         <Link href="/">
+//           <a>Home</a>
+//         </Link>{' '}
+//         |{' '}
+//         <Link href="/about">
+//           <a>About</a>
+//         </Link>{' '}
+//         |{' '}
+//         <Link href="/users">
+//           <a>Users List</a>
+//         </Link>{' '}
+//         | <a href="/api/users">Users API</a>
+//       </nav>
+//     </header>
+//     {children}
+//     <footer>
+//       <hr />
+//       <span>I'm here to stay (Footer)</span>
+//     </footer>
+//   </div>
+// )
 
-export default Layout
+export default function index({ children, title = 'This is the default title' }: Props): ReactElement {
+  
+  const [collapsed, setCollapsed] = useState(false)
+
+  const onCollapse = ()  => setCollapsed(!collapsed)
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={onCollapse}
+        >
+      </Sider>
+      <Layout>
+        <Header style={{ background: '#FFFFFF'}}>
+          <Menu />
+        </Header>
+        <Content>
+        {children}
+        </Content>
+        <Footer>
+          <hr />
+          <span><b>(주)제이든컨설팅</b> 대표자 : 제이든 사업자등록번호 : 000-00-00000</span><br />
+          <span>서울특X시 서X구 서초중X로2X길 2X (서X동, 골X빌딩) X층</span><br />
+          <span>TEL : 00-000-0000 FAX : 00-000-0000 E-mail : seokjoon.yun@gmail.com</span><br />
+          <span>Copyright© 2021 Jayden Consulting. All Rights Reserved.</span><br />
+          
+        </Footer>
+      </Layout>
+    </Layout>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,23 @@
 import Link from 'next/link'
-import Head from 'next/head'
-import { Button, Layout } from 'antd'
-import { ReactElement, useState } from 'react'
+import { Button } from 'antd'
+import { ReactElement } from 'react'
 import { COMPANIES } from '../graphql/companies'
 import { useQuery } from '@apollo/client'
-import Menu from '@components/Menu'
-
-const { Sider, Header, Footer, Content } = Layout
+import Layout from '@components/Layout'
 
 export default function index(): ReactElement {
   const { loading, error, data } = useQuery(COMPANIES)
   console.log(loading, error, data)
 
-  const [collapsed, setCollapsed] = useState(false)
-
-  const onCollapse = ()  => setCollapsed(!collapsed)
-
   return (
-    
-    <Layout style={{ minHeight: '100vh' }}>
-      <Head>
-        <title>{"Home | Next.js + TypeScript Example"}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Sider
-        collapsible
-        collapsed={collapsed}
-        onCollapse={onCollapse}
-        >
-      </Sider>
-      <Layout>
-        <Header style={{ background: '#FFFFFF'}}>
-          <Menu />
-        </Header>
-        <Content>
-          <h1 className="title">Hello Next.js 👋</h1>
-          <Button type="primary">It's Antd Button</Button>
-          <p>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </p>
-        </Content>
-        <Footer>
-          <hr />
-          <span><b>(주)제이든컨설팅</b> 대표자 : 제이든 사업자등록번호 : 000-00-00000</span><br />
-          <span>서울특X시 서X구 서초중X로2X길 2X (서X동, 골X빌딩) X층</span><br />
-          <span>TEL : 00-000-0000 FAX : 00-000-0000 E-mail : seokjoon.yun@gmail.com</span><br />
-          <span>Copyright© 2021 Jayden Consulting. All Rights Reserved.</span><br />
-          
-        </Footer>
-      </Layout>
+    <Layout title='Socio'>
+      <h1 className="title">Hello Next.js 👋</h1>
+      <Button type="primary">It's Antd Button</Button>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
     </Layout>
   )
 }
